40aca6cae44145cb7d20eef3d97ddf2124eb9df0
Jonas Bernoulli <jonas@bernoul.li>
2024-11-03 18:33:51 +0100

    Move copyright to the conventional location

--- ninja-mode.el
+++ ninja-mode.el
@@ -1,11 +1,11 @@
 ;;; ninja-mode.el --- Major mode for editing .ninja files -*- lexical-binding: t -*-
 
+;; Copyright 2011 Google Inc. All Rights Reserved.
+
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: languages
 ;; URL: https://ninja-build.org/
 
-;; Copyright 2011 Google Inc. All Rights Reserved.
-;;
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
 ;; You may obtain a copy of the License at
