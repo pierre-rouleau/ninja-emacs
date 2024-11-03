4e69904954cc8e8daeaf9754b94e012c12334055
Jonas Bernoulli <jonas@bernoul.li>
2024-11-03 18:34:27 +0100

    Update URL header

--- ninja-mode.el
+++ ninja-mode.el
@@ -4,7 +4,7 @@
 
 ;; Package-Requires: ((emacs "24"))
 ;; Keywords: languages
-;; URL: https://ninja-build.org/
+;; URL: https://github.com/jhasse/ninja-emacs
 
 ;; Licensed under the Apache License, Version 2.0 (the "License");
 ;; you may not use this file except in compliance with the License.
